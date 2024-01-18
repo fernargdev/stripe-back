24c0333586767d83cb4e818b72547a3101875e32
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-01-18 16:26:19 -0500

    funciona bien y esta preparado para las pruebas con el front

--- controllers/payments.js
+++ controllers/payments.js
@@ -15,31 +15,31 @@ paymentsRouter.get('/api/notes', (req, res) => {
 // POSTS
 paymentsRouter.post('/api/pay', async (req, res) => {
   try {
-    console.log(req.body[0])
+    console.log(req.body)
 
-    const payment = req.body[0]
+    const payment = req.body
 
     // Crear la descripción del booking
     let description = `Pago para ${payment.company} con idc ${payment.idc} y idf ${payment.idf}`
     let name = `Pago - $${payment.total}`
 
+    // Stripe Checkout
+    const stripeCheckout = stripe(config.STRIPE_TEST_SECRET).checkout
+
     //   Create Checkout Session
-    const session = await stripe(
-      config.STRIPE_TEST_SECRET
-    ).checkout.sessions.create({
+    const session = await stripeCheckout.sessions.create({
       payment_method_types: ['card'],
       mode: 'payment',
       success_url: `https://www.iracubacrm.com/DPage.aspx?Key=26554&
-    idc=${payment.idc}&idf=${payment.idf}&idcp=1&company=${payment.company}`,
+      idc=${payment.idc}&idf=${payment.idf}&idcp=1&company=${payment.company}`,
       cancel_url: `https://www.iracubacrm.com/DPage.aspx?Key=26554&
-    idc=${payment.idc}&idf=${payment.idf}&idcp=0&company=${payment.company}`,
+      idc=${payment.idc}&idf=${payment.idf}&idcp=0&company=${payment.company}`,
       line_items: [
         {
           price_data: {
             currency: 'usd',
             product_data: {
               name: name,
-              // Agregar la descripción a los datos del producto
               description: description,
             },
             unit_amount: payment.total * 100,
@@ -52,8 +52,8 @@ paymentsRouter.post('/api/pay', async (req, res) => {
     res.json(session.url)
   } catch (error) {
     return res.status(400).json({
-      error: error.message,
-      message: req.body[0].company,
+      error: error,
+      message: req.body,
     })
   }
 })
