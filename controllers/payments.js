183497b63181df8025647fb916a030a98c54833b
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-01-18 14:25:10 -0500

    devolver todas las notas de prueba

--- controllers/payments.js
+++ controllers/payments.js
@@ -1,7 +1,12 @@
 const paymentsRouter = require('express').Router()
+const notes = require('../models/notes')
 
 paymentsRouter.get('/', (req, res) => {
   res.send('<h1>API</h1>')
+})
+
+paymentsRouter.get('/api/notes', (req, res) => {
+  res.json(notes)
 })
 
 module.exports = paymentsRouter
