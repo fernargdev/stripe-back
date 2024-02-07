6cea4594f63dc33ecf71437ce615a10cde69d731
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-02-06 22:07:55 -0500

    creando nuevo endpoint para pagos desde la tienda

--- controllers/payments.js
+++ controllers/payments.js
@@ -58,4 +58,49 @@ paymentsRouter.post('/api/pay', async (req, res) => {
   }
 })
 
+paymentsRouter.post('/api/shop', async (req, res) => {
+  try {
+    console.log(req.body)
+
+    const payment = req.body
+
+    // Crear la descripción del booking
+    let description = `Pago para ${payment.company} con idc ${payment.idc} y idf ${payment.idf}`
+    let name = `Pago - $${payment.total}`
+
+    // Stripe Checkout
+    const stripeCheckout = stripe(config.STRIPE_PRODUCTION_SECRET).checkout
+
+    //   Create Checkout Session
+    const session = await stripeCheckout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+      success_url: `https://www.iracubacrm.com/DPage.aspx?Key=26600&
+      idc=${payment.idc}&idf=${payment.idf}&idcp=1&company=${payment.company}`,
+      cancel_url: `https://www.iracubacrm.com/DPage.aspx?Key=26600&
+      idc=${payment.idc}&idf=${payment.idf}&idcp=0&company=${payment.company}`,
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: name,
+              description: description,
+            },
+            unit_amount: Math.round(payment.total * 100),
+          },
+          quantity: 1,
+        },
+      ],
+    })
+
+    res.json(session.url)
+  } catch (error) {
+    return res.status(400).json({
+      error: error,
+      message: req.body,
+    })
+  }
+})
+
 module.exports = paymentsRouter
